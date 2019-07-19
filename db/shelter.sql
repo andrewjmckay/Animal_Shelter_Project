5f87491f39e833c0d8850a0f813fa1128fc10852
andyymckay <andymckay1992@hotmail.co.uk>
2019-07-19 14:38:20 +0100

    tables created

---
+++ db/shelter.sql
@@ -0,0 +1,19 @@
+DROP TABLE animals;
+DROP TABLE owner;
+
+CREATE TABLE owner (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+);
+
+CREATE TABLE animals (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  type VARCHAR(255),
+  breed VARCHAR(255),
+  ready_to_adopt boolean,
+  admission_date VARCHAR(255),
+  owner_id SERIAL4 REFERENCES owner(id) ON DELETE CASCADE,
+);
+
+-- what is delete cascade doing?
