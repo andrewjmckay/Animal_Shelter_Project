9c150c4e5cc4e29748550519a8a1136671f039c9
andyymckay <andymckay1992@hotmail.co.uk>
2019-07-21 19:51:10 +0100

    added empty array instance variable of pets to owner class

--- models/owner.rb
+++ models/owner.rb
@@ -3,11 +3,12 @@ require_relative('../db/sql_runner.rb')
 class Owner
 
   attr_reader :id
-  attr_accessor :first_name, :last_name, :house, :age
+  attr_accessor :name, :pet
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
     @name = options['name']
+    @pet = []
   end
 
 end
