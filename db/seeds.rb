d8c48a33935dd4297d3d472b3f94cdecd7a3edfe
andyymckay <andymckay1992@hotmail.co.uk>
2019-07-21 13:54:42 +0100

    Increased animal objects from 4 to 8

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ require_relative('../models/owner.rb')
 require('pry')
 
 animal1 = Animal.new({
-  'name' => 'Fluffy',
+  'name' => 'Bolt',
   'type' => 'Dog'
   'breed' => 'Australian Shephard',
   'admission_date' => '23.04.19',
@@ -13,7 +13,7 @@ animal1 = Animal.new({
   animal1.save()
 
 animal2 = Animal.new({
-  'name' => 'Kitty',
+  'name' => 'Garfield',
   'type' => 'Cat'
   'breed' => 'British Shorthair',
   'admission_date' => '08.02.19',
@@ -23,7 +23,7 @@ animal2 = Animal.new({
   animal2.save()
 
 animal3 = Animal.new({
-  'name' => 'Whiskers',
+  'name' => 'Simba',
   'type' => 'Cat'
   'breed' => 'Scottish Fold',
   'admission_date' => '11.03.19',
@@ -33,13 +33,52 @@ animal3 = Animal.new({
   animal3.save()
 
 animal4 = Animal.new({
-  'name' => 'Doggo',
+  'name' => 'Dug',
   'type' => 'Dog'
   'breed' => 'Siberian Huskey',
   'admission_date' => '03.06.19',
   'ready_for_adoption' => true
   })
   animal4.save()
+
+  animal5 = Animal.new({
+    'name' => 'Gromit',
+    'type' => 'Dog'
+    'breed' => 'Pomeranian',
+    'admission_date' => '29.02.19',
+    'ready_for_adoption' => false
+    })
+
+    animal5.save()
+
+  animal6 = Animal.new({
+    'name' => 'Thomas O'Malley',
+    'type' => 'Cat'
+    'breed' => 'Ragamuffin',
+    'admission_date' => '09.05.19',
+    'ready_for_adoption' => true
+    })
+
+    animal6.save()
+
+  animal7 = Animal.new({
+    'name' => 'Grumpy Cat',
+    'type' => 'Cat'
+    'breed' => 'Ragdoll',
+    'admission_date' => '13.06.19',
+    'ready_for_adoption' => false
+    })
+
+    animal7.save()
+
+  animal8 = Animal.new({
+    'name' => 'Scooby-Doo',
+    'type' => 'Dog'
+    'breed' => 'Akita Inu',
+    'admission_date' => '25.02.19',
+    'ready_for_adoption' => true
+    })
+    animal8.save()
 
 binding.pry
 nil
