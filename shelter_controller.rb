b15156f7cdc5aab8c92396581d4b3548bee40f19
andyymckay <andymckay1992@hotmail.co.uk>
2019-07-23 11:24:03 +0100

    created animals.erb/owners.erb. added get methods to controller

--- shelter_controller.rb
+++ shelter_controller.rb
@@ -1,5 +1,29 @@
 require('sinatra')
 require('sinatra/reloader')
+# require('pry-byebug')
+# difference between require pry and require prybug
 require_relative('./models/owner.rb')
 require_relative('./models/animal.rb')
 also_reload('./models/*')
+
+
+# home
+get '/' do
+  erb(:index)
+end
+
+# show animals
+
+get '/shelter/animals' do
+  @animals = Animal.all()
+  erb(:animals)
+end
+
+# show owners
+
+get '/shelter/owners' do
+  @owners = Owner.all
+  erb(:owners)
+end
+
+# assign animal to new owner
